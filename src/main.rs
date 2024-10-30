eaf4acacdf83fd45bb3ebce61ad362d94c2bcfd8
jerrysxie <>
2024-10-30 13:12:03 -0500

    Update to latesst

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Set the I2C slave address to the device we're communicating with.
     i2c.set_slave_address(ADDR)?;
 
-    let init_cmd = [0xDE, 0xAD, 0xBE, 0xEF];
-    let magic_cmd = [0xDE, 0xCA, 0xFB, 0xAD];
+    let init_cmd = [0x05, 0xDE, 0xAD, 0xBE, 0xEF];
+    let magic_cmd = [0x05, 0xDE, 0xCA, 0xFB, 0xAD];
 
+    let mut i: u32 = 0;
     loop {
-        i2c.write(&init_cmd)?;
-
-        let mut code: [u8; 4] = [0xAA; 4];
-        i2c.write_read(&magic_cmd, &mut code)?;
-
-        println!("magic code = {:02x?}", code);
-        thread::sleep(Duration::from_secs(1));
+        if i % 2 == 0 {
+            println!("write init cmd");
+            i2c.write(&init_cmd)?;
+        } else {
+            let mut code: [u8; 4] = [0xAA; 4];
+            println!("write magic code");
+            i2c.write_read(&magic_cmd, &mut code)?;
+            println!("magic code = {:02x?}", code);
+        }
+        i += 1;
+        thread::sleep(Duration::from_secs(2));
     }
 }
